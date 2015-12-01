ffef31af74ce8773f195e76bc792b0d0e85b05d4
Michele-Simplon <samtortue@gmail.com>
2015-12-01 15:18:55 +0100

    version 1

---
+++ script-base.js
@@ -0,0 +1,28 @@
+function byId (elementId){
+  return.document.getElementById('elementId');
+  if (typeOf elementId ="string" ){
+    console.log("Erreur: elementId doit être une chaine de caractère")
+    return null;
+  }
+
+}
+
+function byClass(className, atIndex){
+  var elements = document.getElementsByClassName(className);
+  var tElement.Array.prototype.slice.call(elements);
+  return (atIndex !==null) ? tElements[atIndex]
+
+}
+
+function byTag (Tag){
+  return.document.getElementsByTagName(Tag);
+
+}
+
+function addClass(){
+return.
+}
+
+function removeClass(){
+
+}
